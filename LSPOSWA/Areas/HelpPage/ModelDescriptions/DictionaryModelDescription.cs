0456ce6cde2405c1d50149580722d7682859f7f2
licovali <vlicovali@gmail.com>
2014-08-19 15:57:04 -0500

    Initial commit
    Initial commit

---
+++ LSPOSWA/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LSPOSWA.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LSPOSWA/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LSPOSWA.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
